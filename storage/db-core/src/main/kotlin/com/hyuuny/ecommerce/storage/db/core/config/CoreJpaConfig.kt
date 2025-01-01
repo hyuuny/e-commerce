99fe8343304684cecc6f482c6f3e129321436c43
hyuuny <shyune@knou.ac.kr>
2025-01-01 12:30:03 +0900

    feat: DataSource 설정

---
+++ storage/db-core/src/main/kotlin/com/hyuuny/ecommerce/storage/db/core/config/CoreJpaConfig.kt
@@ -0,0 +1,12 @@
+package com.hyuuny.ecommerce.storage.db.core.config
+
+import org.springframework.boot.autoconfigure.domain.EntityScan
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+import org.springframework.transaction.annotation.EnableTransactionManagement
+
+@Configuration
+@EnableTransactionManagement
+@EntityScan(basePackages = ["com.hyuuny.ecommerce.storage.db.core"])
+@EnableJpaRepositories(basePackages = ["com.hyuuny.ecommerce.storage.db.core"])
+internal class CoreJpaConfig
