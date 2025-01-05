f56006e276785b4ce880589524453e5b1a1a81a1
hyuuny <shyune@knou.ac.kr>
2025-01-05 14:17:29 +0900

    feat: 카테고리 조회 기능 cache 적용

--- core-api/build.gradle.kts
+++ core-api/build.gradle.kts
@@ -9,6 +9,7 @@ tasks.getByName("jar") {
 dependencies {
     implementation(project(":support:logging"))
     implementation(project(":storage:db-core"))
+    implementation(project(":core-redis"))
 
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-security")
@@ -16,4 +17,5 @@ dependencies {
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.3")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.3")
     testImplementation("io.rest-assured:kotlin-extensions:5.2.0")
+    testImplementation("org.testcontainers:testcontainers:1.17.2")
 }
--- core-api/src/main/kotlin/com/hyuuny/ecommerce/core/api/v1/categories/CategoryService.kt
+++ core-api/src/main/kotlin/com/hyuuny/ecommerce/core/api/v1/categories/CategoryService.kt
@@ -1,16 +1,24 @@
 package com.hyuuny.ecommerce.core.api.v1.categories
 
+import org.springframework.cache.annotation.CacheEvict
+import org.springframework.cache.annotation.Cacheable
 import org.springframework.stereotype.Service
 
 @Service
 class CategoryService(
     private val categoryReader: CategoryReader,
 ) {
+    @Cacheable(value = ["parentCategories"], key = "#root.method.name")
     fun getAllParentCategory(): List<ParentCategoryData> =
         categoryReader.readAllParentCategory().filterNot { it.hide }
             .map { ParentCategoryData(it) }
 
+    @Cacheable(value = ["childrenCategories"], key = "#parentId", unless = "#result.isEmpty()")
     fun getAllChildrenCategory(parentId: Long): List<ChildCategoryData> =
         categoryReader.readAllChildrenCategory(parentId).filterNot { it.hide }
             .map { ChildCategoryData(it) }
+
+    @CacheEvict(value = ["parentCategories", "childrenCategories"], allEntries = true)
+    fun cacheEvict() {
+    }
 }
