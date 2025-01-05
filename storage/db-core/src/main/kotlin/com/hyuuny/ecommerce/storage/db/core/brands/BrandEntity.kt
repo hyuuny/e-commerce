048f4d2db6973c6df2779051542bd179ebc7ed93
hyuuny <shyune@knou.ac.kr>
2025-01-05 20:48:40 +0900

    feat: CategoryEntity 구현

---
+++ storage/db-core/src/main/kotlin/com/hyuuny/ecommerce/storage/db/core/brands/BrandEntity.kt
@@ -0,0 +1,13 @@
+package com.hyuuny.ecommerce.storage.db.core.brands
+
+import com.hyuuny.ecommerce.storage.db.core.BaseEntity
+import jakarta.persistence.Entity
+import jakarta.persistence.Table
+
+@Table(name = "brands")
+@Entity
+class BrandEntity(
+    val nameKo: String,
+    val nameEn: String,
+    val imageUrl: String,
+) : BaseEntity()
