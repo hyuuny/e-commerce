155cdb29ed0f1e68639637074f8c35d3cabf3406
hyuuny <shyune@knou.ac.kr>
2025-01-05 20:49:45 +0900

    feat: QueryDSL 의존성 추가

--- storage/db-core/build.gradle.kts
+++ storage/db-core/build.gradle.kts
@@ -8,4 +8,8 @@ dependencies {
     api("org.springframework.boot:spring-boot-starter-data-jpa")
     runtimeOnly("com.mysql:mysql-connector-j")
     runtimeOnly("com.h2database:h2")
+    implementation("com.querydsl:querydsl-jpa:${property("querydslVersion")}:jakarta")
+    implementation("com.querydsl:querydsl-apt:${property("querydslVersion")}:jakarta")
+    implementation("jakarta.persistence:jakarta.persistence-api")
+    kapt("com.querydsl:querydsl-apt:${property("querydslVersion")}:jakarta")
 }
