b70c4f61a02af543236d509e5bf083a19dc14939
hyuuny <shyune@knou.ac.kr>
2025-01-06 18:41:20 +0900

    test: 브랜드 조회 및 검색 기능 테스트 구현

---
+++ core-api/src/test/kotlin/com/hyuuny/ecommerce/core/api/v1/brands/BrandServiceTest.kt
@@ -0,0 +1,65 @@
+package com.hyuuny.ecommerce.core.api.v1.brands
+
+import com.hyuuny.ecommerce.storage.db.core.brands.BrandEntity
+import com.hyuuny.ecommerce.storage.db.core.response.SimplePage
+import io.mockk.every
+import io.mockk.mockk
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.springframework.data.domain.PageRequest
+
+class BrandServiceTest {
+    private lateinit var reader: BrandReader
+    private lateinit var service: BrandService
+
+    @BeforeEach
+    fun setUp() {
+        reader = mockk()
+        service = BrandService(reader)
+        service.cacheEvict()
+    }
+
+    @Test
+    fun `브랜드 목록을 조회할 수 있다`() {
+        val brands = listOf(
+            BrandEntity("메디힐", "mediheal", "brands/images/mediheal.png"),
+            BrandEntity("토리든", "Torriden", "brands/images/Torriden.png"),
+            BrandEntity("달바", "dalba", "brands/images/dalba.png"),
+            BrandEntity("필리밀리", "Fillimilli", "brands/images/Fillimilli.png"),
+            BrandEntity("닥터지", "Dr.G", "brands/images/Dr.G.png"),
+            BrandEntity("라운드랩", "Round Lab", "brands/images/Round-Lab.png"),
+            BrandEntity("어노브", "UNOVE", "brands/images/UNOVE.png"),
+            BrandEntity("에스트라", "aesura", "brands/images/aesura.png"),
+            BrandEntity("넘버즈인", "NIMBUZIN", "brands/images/NIMBUZIN.png"),
+            BrandEntity("바이오더마", "BIODERMA", "brands/images/BIODERMA.png"),
+        )
+        val page = SimplePage(brands, 1, 10, true)
+        every { reader.findAllBySearch(any(), any()) } returns page
+
+        val search = service.search(BrandSearchCommand(), PageRequest.of(0, 10))
+
+        assertThat(search.content).hasSize(10)
+        search.content.forEachIndexed { index, item ->
+            assertThat(item.nameKo).isEqualTo(brands[index].nameKo)
+            assertThat(item.nameEn).isEqualTo(brands[index].nameEn)
+        }
+    }
+
+    @Test
+    fun `브랜드 이름으로 검색할 수 있다`() {
+        val brands = listOf(
+            BrandEntity("닥터지", "Dr.G", "brands/images/Dr.G.png"),
+        )
+        val page = SimplePage(brands, 1, 10, true)
+        every { reader.findAllBySearch(any(), any()) } returns page
+
+        val search = service.search(BrandSearchCommand("닥터"), PageRequest.of(0, 10))
+
+        assertThat(search.content).hasSize(1)
+        search.content.forEachIndexed { index, item ->
+            assertThat(item.nameKo).isEqualTo(brands[index].nameKo)
+            assertThat(item.nameEn).isEqualTo(brands[index].nameEn)
+        }
+    }
+}
