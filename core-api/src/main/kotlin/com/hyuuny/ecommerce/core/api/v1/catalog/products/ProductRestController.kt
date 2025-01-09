e87680a6c9c50339d194ff1085252ae9cee0f8ac
hyuuny <shyune@knou.ac.kr>
2025-01-09 18:03:10 +0900

    feat: 상품 조회 및 검색 기능 api 구현

---
+++ core-api/src/main/kotlin/com/hyuuny/ecommerce/core/api/v1/catalog/products/ProductRestController.kt
@@ -0,0 +1,25 @@
+package com.hyuuny.ecommerce.core.api.v1.catalog.products
+
+import com.hyuuny.ecommerce.core.support.response.ApiResponse
+import com.hyuuny.ecommerce.storage.db.core.response.SimplePage
+import org.springframework.data.domain.Pageable
+import org.springframework.data.domain.Sort
+import org.springframework.data.web.PageableDefault
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RequestMapping("/api/v1/products")
+@RestController
+class ProductRestController(
+    private val service: ProductService,
+) {
+    @GetMapping
+    fun search(
+        request: ProductSearchRequestDto,
+        @PageableDefault(sort = ["id"], direction = Sort.Direction.DESC) pageable: Pageable
+    ): ApiResponse<SimplePage<ProductResponseDto>> {
+        val page = service.search(request.toCommand(), pageable)
+        return ApiResponse.success(SimplePage(page.content.map { ProductResponseDto(it) }, page))
+    }
+}
