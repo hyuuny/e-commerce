09d9b1c92ff859de18baa8efab0b59e8cec4a9f9
hyuuny <shyune@knou.ac.kr>
2025-01-11 13:11:21 +0900

    feat: CategoryProductEntity 유니크 키 추가

--- storage/db-core/src/main/kotlin/com/hyuuny/ecommerce/storage/db/core/catalog/categories/CategoryProductEntity.kt
+++ storage/db-core/src/main/kotlin/com/hyuuny/ecommerce/storage/db/core/catalog/categories/CategoryProductEntity.kt
@@ -4,9 +4,13 @@ import com.hyuuny.ecommerce.storage.db.core.BaseEntity
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.Table
+import jakarta.persistence.UniqueConstraint
 
 @Entity
-@Table(name = "category_products")
+@Table(
+    name = "category_products",
+    uniqueConstraints = [UniqueConstraint(columnNames = ["category_id", "product_id"])]
+)
 class CategoryProductEntity(
     @Column(nullable = false) val categoryId: Long,
     @Column(nullable = false) val productId: Long,
