6689757ad1aa8499b4e34343a486f1457f39be4e
hyuuny <shyune@knou.ac.kr>
2025-01-14 19:58:48 +0900

    test: 주문 상세 조회 api 테스트 구현

--- core-api/src/test/kotlin/com/hyuuny/ecommerce/core/api/v1/orders/OrderRestControllerTest.kt
+++ core-api/src/test/kotlin/com/hyuuny/ecommerce/core/api/v1/orders/OrderRestControllerTest.kt
@@ -146,6 +146,88 @@ class OrderRestControllerTest(
         }
     }
 
+    @Test
+    fun `주문 내역을 상세조회 할 수 있다`() {
+        val productEntities = generateProductEntities()
+        val checkoutItems = listOf(
+            CheckoutItem(
+                productId = productEntities[0].id,
+                quantity = 1,
+                discountAmount = productEntities[0].discountPrice.discountAmount * 1,
+                amount = productEntities[0].price.amount * 1,
+                totalAmount = productEntities[0].calculateTotalPrice() * 1
+            ),
+            CheckoutItem(
+                productId = productEntities[1].id,
+                quantity = 2,
+                discountAmount = productEntities[1].discountPrice.discountAmount * 2,
+                amount = productEntities[1].price.amount * 2,
+                totalAmount = productEntities[1].calculateTotalPrice() * 2
+            ),
+        )
+        val totalProductPrice = checkoutItems[0].amount + checkoutItems[1].amount
+        val totalDiscountAmount = checkoutItems[0].discountAmount + checkoutItems[1].discountAmount
+        val shippingFee = 3000L
+        val totalPrice = totalProductPrice - totalDiscountAmount + shippingFee
+        val command = Checkout(
+            userId = 1,
+            orderer = OrdererCommand(
+                name = "김성현",
+                phoneNumber = "01012341234"
+            ),
+            deliveryDetail = DeliveryDetailCommand(
+                address = "서울시 구로구 123",
+                addressDetail = "3동 503호",
+                recipientName = "김성현",
+                message = "문앞 보관해주세요!"
+            ),
+            totalProductAmount = totalProductPrice,
+            totalDiscountAmount = totalDiscountAmount,
+            shippingFee = shippingFee,
+            totalAmount = totalPrice,
+            items = checkoutItems
+        )
+        val newOrder = service.checkout(command)
+
+        Given {
+            contentType(ContentType.JSON)
+            header(HttpHeaders.AUTHORIZATION, generateJwtToken(DEFAULT_USER_EMAIL, DEFAULT_USER_PASSWORD))
+            log().all()
+        } When {
+            get("/api/v1/orders/${newOrder.id}")
+        } Then {
+            statusCode(HttpStatus.SC_OK)
+            body("result", equalTo(ResultType.SUCCESS.name))
+            body("data.id", equalTo(newOrder.id.toInt()))
+            body("data.orderCode", equalTo(newOrder.orderCode))
+            body("data.status", equalTo(newOrder.status.name))
+            body("data.userId", equalTo(newOrder.userId.toInt()))
+            body("data.orderer.name", equalTo(newOrder.orderer.name))
+            body("data.orderer.phoneNumber", equalTo(newOrder.orderer.phoneNumber))
+            body("data.deliveryDetail.address", equalTo(newOrder.deliveryDetailData.address))
+            body("data.deliveryDetail.addressDetail", equalTo(newOrder.deliveryDetailData.addressDetail))
+            body("data.deliveryDetail.recipientName", equalTo(newOrder.deliveryDetailData.recipientName))
+            body("data.deliveryDetail.message", equalTo(newOrder.deliveryDetailData.message))
+            body("data.totalProductAmount", equalTo(newOrder.totalProductPrice.totalProductAmount.toInt()))
+            body("data.totalDiscountAmount", equalTo(newOrder.totalDiscountPrice.totalDiscountAmount.toInt()))
+            body("data.shippingFee", equalTo(newOrder.shippingFee.toInt()))
+            body("data.totalAmount", equalTo(newOrder.totalPrice.totalAmount.toInt()))
+            body("data.items[0].productId", equalTo(newOrder.items[0].productId.toInt()))
+            body("data.items[0].productName", equalTo(productEntities[0].name))
+            body("data.items[0].quantity", equalTo(newOrder.items[0].quantity.toInt()))
+            body("data.items[0].discountAmount", equalTo(newOrder.items[0].discountPrice.discountAmount.toInt()))
+            body("data.items[0].amount", equalTo(newOrder.items[0].price.amount.toInt()))
+            body("data.items[0].totalAmount", equalTo(productEntities[0].calculateTotalPrice().times(1).toInt()))
+            body("data.items[1].productId", equalTo(newOrder.items[1].productId.toInt()))
+            body("data.items[1].productName", equalTo(productEntities[1].name))
+            body("data.items[1].quantity", equalTo(newOrder.items[1].quantity.toInt()))
+            body("data.items[1].discountAmount", equalTo(newOrder.items[1].discountPrice.discountAmount.toInt()))
+            body("data.items[1].amount", equalTo(newOrder.items[1].price.amount.toInt()))
+            body("data.items[1].totalAmount", equalTo(productEntities[1].calculateTotalPrice().times(2).toInt()))
+            log().all()
+        }
+    }
+
     private fun generateCheckoutRequest(checkoutItems: List<CheckoutItemRequestDto>): CheckoutRequestDto {
         val totalProductPrice = checkoutItems[0].amount + checkoutItems[1].amount
         val totalDiscountAmount = checkoutItems[0].discountAmount + checkoutItems[1].discountAmount
