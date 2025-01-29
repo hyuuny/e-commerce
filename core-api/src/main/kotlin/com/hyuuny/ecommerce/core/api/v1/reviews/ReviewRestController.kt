cf1e4b8afa836bf98fae4907279d1cff2ba7ecac
hyuuny <shyune@knou.ac.kr>
2025-01-29 19:41:13 +0900

    feat: 후기 목록 및 검색 기능 api 구현

--- core-api/src/main/kotlin/com/hyuuny/ecommerce/core/api/v1/reviews/ReviewRestController.kt
+++ core-api/src/main/kotlin/com/hyuuny/ecommerce/core/api/v1/reviews/ReviewRestController.kt
@@ -1,6 +1,10 @@
 package com.hyuuny.ecommerce.core.api.v1.reviews
 
 import com.hyuuny.ecommerce.core.support.response.ApiResponse
+import com.hyuuny.ecommerce.storage.db.core.response.SimplePage
+import org.springframework.data.domain.Pageable
+import org.springframework.data.domain.Sort
+import org.springframework.data.web.PageableDefault
 import org.springframework.web.bind.annotation.*
 
 @RequestMapping("/api/v1/reviews")
@@ -18,5 +22,14 @@ class ReviewRestController(
     fun getReview(@PathVariable id: Long): ApiResponse<ReviewViewResponseDto> {
         val review = service.getReview(id)
         return ApiResponse.success(ReviewViewResponseDto(review))
+    }
+
+    @GetMapping
+    fun search(
+        request: ReviewSearchRequestDto,
+        @PageableDefault(sort = ["id"], direction = Sort.Direction.DESC) pageable: Pageable,
+    ): ApiResponse<SimplePage<ReviewResponseDto>> {
+        val page = service.search(request.toCommand(), pageable)
+        return ApiResponse.success(SimplePage(page.content.map { ReviewResponseDto(it) }, page))
     }
 }
