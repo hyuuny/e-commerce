18c60c0f963ec7a710277c67925ae2dcfd5079d7
hyuuny <shyune@knou.ac.kr>
2025-03-23 18:38:52 +0900

    feat: poi excel 의존성 추가

--- core-api/build.gradle.kts
+++ core-api/build.gradle.kts
@@ -17,6 +17,9 @@ dependencies {
     implementation("io.jsonwebtoken:jjwt-api:0.12.3")
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.3")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.3")
+    implementation("org.apache.poi:poi:5.2.3")
+    implementation("org.apache.poi:poi-ooxml:5.2.3")
+
     testImplementation("io.rest-assured:kotlin-extensions:5.2.0")
     testImplementation("org.testcontainers:testcontainers:1.17.2")
 }
